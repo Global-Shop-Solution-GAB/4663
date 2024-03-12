c02c0fdb7b09fc860cf29d1ec43deba50f65f241
Service Web <sw@gssmail.com>
2024-03-12 12:31:57 -0500

    Commit changes in C:\Websites\Git\emarlow\4663\GAB_4663_PULSE_SALE.sql

---
+++ GAB_4663_PULSE_SALE.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "GAB_4663_PULSE_SALE"(
+ "Category" CHAR(50),
+ "Mth_Budget" INTEGER,
+ "Mth_Confirmed" INTEGER,
+ "Mth_Shipped" INTEGER,
+ "Mth_Total" INTEGER,
+ "Ytd_Budget" INTEGER,
+ "Ytd_Actual" INTEGER,
+ "Ytd_Variance" DOUBLE,
+ "GP_Target" DOUBLE,
+ "GP_Actual" NUMERIC(8,2),
+ "Date_Run" DATE);
